ed221dc8400b1efb73118bc8c35c0f88df6d9efa
lzy00codeforfun <541908562@qq.com>
2021-12-05 00:04:11 -0500

    bwy Add Vue & Flask

---
+++ frontend/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <img src="./assets/DataCube.png"
+      height="100"
+  >
+  <div>
+  </div>
+  <HashtagRank msg="Welcome to Your Vue.js App"/>
+  <SearchTool/>
+</template>
+
+<script>
+import HashtagRank from './components/HashtagRank.vue'
+import SearchTool from './components/SearchTool.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HashtagRank,
+    SearchTool
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: -10px;
+}
+</style>
